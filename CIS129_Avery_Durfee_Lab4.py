ba08755ab9808d3921673f6c390e42289554fe83
AveryDurfee <159318160+AveryDurfee@users.noreply.github.com>
2024-03-05 15:48:04 -0700

    Create CIS129_Avery_Durfee_Lab4.py

---
+++ CIS129_Avery_Durfee_Lab4.py
@@ -0,0 +1,16 @@
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+storeAmount = 0  # percent of sales increase
+prompt = ?????? # prompt will be a string literal
+
+monthlySales = float(input(prompt))
+if monthlySales >= 110000:
+		storeAmount = 6000
+  elif monthlySales >= 100000: 
+    
+	elif 
+	elif 
+	else:
+    
