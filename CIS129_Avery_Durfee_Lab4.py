7d1f338f72c86b0bdfddeea5768f711807a4d3d3
AveryDurfee <159318160+AveryDurfee@users.noreply.github.com>
2024-03-05 16:23:46 -0700

    Update CIS129_Avery_Durfee_Lab4.py

--- CIS129_Avery_Durfee_Lab4.py
+++ CIS129_Avery_Durfee_Lab4.py
@@ -3,14 +3,23 @@ monthlySales = 0  # monthly sales amount
 storeAmount = 0  # store bonus amount
 empAmount = 0  # employee bonus amount
 storeAmount = 0  # percent of sales increase
-prompt = ?????? # prompt will be a string literal
+# prompt = ?????? # prompt will be a string literal
 
-monthlySales = float(input(prompt))
+# This code determines the storeAmount bonus
+monthlySales = float(input("How much did you sell"))
 if monthlySales >= 110000:
-		storeAmount = 6000
-  elif monthlySales >= 100000: 
-    
-	elif 
-	elif 
-	else:
-    
+	storeAmount = 6000
+elif monthlySales >= 100000:
+	storeAmount = 5000
+elif monthlySales >= 90000:
+	storeAmount = 4000
+elif monthlySales >= 80000:
+	storeAmount = 3000
+else:
+	storeAmount = 0
+	
+# This code gets the percent of increase in sales
+salesIncrease = float(input("By what percentage))
+salesIncrease = salesIncrease / 100
+if salesIncrease >= .05:
+	empAmount = 75
